1712bb88fbc328ed328fd1cf6ad9a9d65347efda
PrasoonSTGI <164321191+PrasoonSTGI@users.noreply.github.com>
2025-03-06 02:21:23 +0530

    Update recon_stgwe_filemover.sh

--- recon_stgwe_filemover.sh
+++ recon_stgwe_filemover.sh
@@ -167,15 +167,33 @@ recheck_prerequisites() {
 recheck_prerequisites
 
 # Check if the PostgreSQL container is already running
+# Step to validate DB connection only if the container is running
 log_message 'Checking if PostgreSQL container "filemover-db" is already running...'
 docker ps | grep -q "filemover-db"
 if [ $? -eq 0 ]; then
     log_message "PostgreSQL container 'filemover-db' is already running."
 
-    # If the container is already running, validate DB connection
-    authenticate_db
+    # Check if .env-pdi file exists
+    if [ -f /home/$USER/.env-pdi ]; then
+        log_message ".env-pdi file found. Using environment variables for DB connection."
+        # Authenticate using the environment file
+        authenticate_db
+    else
+        log_message ".env-pdi file not found. Using DB credentials directly for validation."
+
+        # Validate DB connection using provided credentials
+        PGPASSWORD=$db_password
+        docker run --rm --network host -v /home/$USER:/home/$USER --env DB_PASSWORD="$db_password" -e PGPASSWORD="$PGPASSWORD" postgres psql --port 15432 --host localhost --username "$db_username" --dbname "$db_name" -c "\q"
+        
+        if [ $? -ne 0 ]; then
+            log_message "Failed to validate DB connection with provided credentials. Exiting script."
+            exit 1
+        else
+            log_message "DB connection validated successfully using provided credentials."
+        fi
+    fi
 else
-    # If the container is not running, prompt the user for credentials and create the container
+    # Container not running, prompt for DB credentials and proceed
     log_message "PostgreSQL container is not running. Proceeding with DB credential input."
     prerequisite_db_credential
 fi
