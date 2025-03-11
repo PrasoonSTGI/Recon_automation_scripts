fe14b71fabce33b3a1bdab83f325db7d0ead15ba
PrasoonSTGI <164321191+PrasoonSTGI@users.noreply.github.com>
2025-03-12 02:46:51 +0530

    Update Non-Docker_migration.sh

--- Non-Docker_migration.sh
+++ Non-Docker_migration.sh
@@ -1,11 +1,25 @@
-#put a pre test in the start explaing what ius script about and also menmtion its should run on current non docker platform
 #!/bin/bash
+
+# This script performs the following operations:
+# 1. Prompts the user to enter database credentials (username, name, and password).
+# 2. Creates a backup of the specified PostgreSQL database using pg_dump.
+# 3. Saves the backup to a specified directory on the local system.
+# 4. Guides the user to transfer the generated dump file to the new production server (Docker platform).
+# 5. Informs the user to fetch and execute the Dockerized migration script to complete the database migration from the non-Docker to the Docker platform.
+# 
+# The script is intended to run on a non-Docker platform, ensuring the PostgreSQL database is backed up locally before the migration.
+# 
+# Make sure to have access to the PostgreSQL database and required user permissions for pg_dump.
+# This script assumes that the PostgreSQL service is running on localhost with the default port 5432.
+#
+# This script should be executed on a non-Docker platform.
 
 db_username=""
 db_name=""
 db_password="" 
 db_host="localhost"
 db_port="5432" 
+
 # Function to print an error message and exit
 handle_error() {
     echo "$1"
@@ -22,35 +36,12 @@ sleep_after_command() {
     sleep 4  # Sleep for 4 seconds (you can adjust the duration)
 }
 
-# Function to check the status of a command
-# check_command_status() {
-#     if [ $? -ne 0 ]; then
-#         log_message "$1 failed. Exiting script."
-#         exit 1
-#     else
-#         log_message "$1 succeeded."
-#     fi
-# }
-
-# Function to validate DB credentials
-validate_db_credentials() {
-    local db_username=$1
-    local db_password=$2
-    local db_name=$3
-    local db_host="localhost"
-    local db_port="5432"
-
-    # Validate DB credentials using psql command
-    PGPASSWORD=$db_password psql -h "$db_host" -U "$db_username" -d "$db_name" -p "$db_port" -c "\q" &>/dev/null
-    return $?  # Return the status of the psql command
-}
-
 # Function to prompt user for database credentials
 prerequisite_db_credentials() {
     echo -e "\e[33mDatabase Credentials \e[0m"
     echo -e "\e[34m################################################################################################################################################### \e[0m"
 
-     echo -e "\e[36mEnter DB username: \e[0m"
+    echo -e "\e[36mEnter DB username: \e[0m"
     read db_username
 
     echo -e "\e[36mEnter DB name: \e[0m"
@@ -61,32 +52,6 @@ prerequisite_db_credentials() {
     echo  
 
     echo -e "\e[34m################################################################################################################################################### \e[0m"
-}
-
-# Function to authenticate DB credentials
-authenticate_db() {
-    prerequisite_db_credentials
-    log_message "DB credentials provided. Validating..."
-
-    local attempts=3
-    while [[ $attempts -gt 0 ]]; do
-        validate_db_credentials "$db_username" "$db_password" "$db_name"
-        if [ $? -eq 0 ]; then
-            echo -e "\e[32mDB Credentials Verified \e[0m"
-            log_message "DB connection successful."
-            return 0
-        else
-            echo -e "\e[31mWrong Credentials!!!! \e[0m"
-            ((attempts--))
-            log_message "DB connection failed. $attempts attempt(s) remaining."
-            if [[ $attempts -eq 0 ]]; then
-                log_message "Failed to authenticate DB credentials after 3 attempts. Exiting script."
-                exit 1
-            fi
-            echo -e "\e[36mPlease re-enter DB credentials.\e[0m"
-            prerequisite_db_credentials  # Prompt user to input DB credentials again
-        fi
-    done
 }
 
 # Function to handle database backup
@@ -102,7 +67,7 @@ perform_db_backup() {
     fi
 
     # Construct the dump file name with the current date
-    local date=$(date "+%Y%m%d") #add time stamp also
+    local date=$(date '+%Y-%m-%d %H:%M:%S')
     local dump_file="/home/$user_dir/archive/DB_Backup/CurrProd_DB_Backup_$date.dump"
 
     # Run the pg_dump command to create the backup
@@ -117,16 +82,22 @@ perform_db_backup() {
     fi
 }
 
-
 echo -e "\e[34m################################################################################################################################################### \e[0m"
 echo -e "\e[33mSCRIPT STARTED.... \e[0m" 
 echo -e "\e[34m################################################################################################################################################### \e[0m"
 
-# Authenticate DB credentials before proceeding
-authenticate_db### not required
-#echo -e "\e[34m################################################################################################################################################### \e[0m"
+prerequisite_db_credentials
+
 # Perform database backup
 perform_db_backup
+
 echo -e "\e[34m################################################################################################################################################### \e[0m"
 echo -e "\e[32mScript completed successfully!!! \e[0m"
-#give an echo statement guiding user that dump is created ftp it to new prod something like that 
+
+# Guide the user about the next steps:
+echo -e "\e[33mNext Steps:\e[0m"
+echo -e "\e[36m1. The database dump file has been created successfully: $dump_file.\e[0m"
+echo -e "\e[36m2. Please transfer this dump file to the new production server (Docker platform).\e[0m"
+echo -e "\e[36m3. After transferring, fetch the 'Dockerized_migration.sh' script from the 'Recon_automation_scripts' repository on the production server.\e[0m"
+echo -e "\e[36m4. Execute the 'Dockerized_migration.sh' script to complete the database migration from the non-Docker platform to the Docker platform.\e[0m"
+echo -e "\e[34m################################################################################################################################################### \e[0m"
